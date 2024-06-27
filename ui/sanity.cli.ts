1cca2a9856862db76d93cb02636f2786ac57954f
jaikechen <31357872+jaikechen@users.noreply.github.com>
2024-06-27 13:47:27 -0400

    containerize ui

--- ui/sanity.cli.ts
+++
@@ -1,9 +0,0 @@
-import { defineCliConfig } from "sanity/cli";
-import { projectId, dataset } from "./lib/sanity/config";
-
-export default defineCliConfig({
-  api: {
-    projectId: projectId,
-    dataset: dataset
-  }
-});
