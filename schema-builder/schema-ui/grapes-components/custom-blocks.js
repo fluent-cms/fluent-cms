91d086f8e5477c480ed7b9ba3d7a134d2925be37
jaikechen <31357872+jaikechen@users.noreply.github.com>
2024-09-24 07:11:00 -0400

    add a link to homepage

--- schema-builder/schema-ui/grapes-components/custom-blocks.js
+++ schema-builder/schema-ui/grapes-components/custom-blocks.js
@@ -75,7 +75,7 @@ export const customBlocks = [
         content: `
  <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+    <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
       </svg>
